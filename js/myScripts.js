65923a1dd9f0f0a6588dfbd8a199d79fdb2dee1a
Robert Manolis <robertpm@comcast.net>
2016-02-09 20:26:27 -0800

    ui re-factor

--- js/myScripts.js
+++ js/myScripts.js
@@ -21,10 +21,12 @@ var myTreehouseProjects = function() {
 	// BOOLEAN TO TRACK NAV OPEN STATE
 	var navOpen = false;
 
+	var arrowWrap = document.getElementsByClassName("arrowWrap");
 	var arrowLeft = document.getElementsByClassName("arrowLeft")[0];
 	var arrowRight = document.getElementsByClassName("arrowRight")[0];
 
 	// PROJ IMAGE VARIBALES
+	var lgProjImage = document.getElementsByClassName("lgProjImage");
 	var smProjImage = document.getElementsByClassName("smProjImage");
 	var projOpen = document.getElementsByClassName("projOpen");
 	var projInfo = document.getElementsByClassName("projInfo");
@@ -34,6 +36,8 @@ var myTreehouseProjects = function() {
 	var projDescriptionWrap = document.getElementsByClassName("projDescriptionWrap");
 	var projTechWrap = document.getElementsByClassName("projTechWrap");
 	var projInfo = document.getElementsByClassName("projInfo");
+	var projSectionTitleAbout = document.getElementsByClassName("projSectionTitleAbout");
+	var projSectionTitleSkills = document.getElementsByClassName("projSectionTitleSkills");
 	
 	// BOOLEAN TO TRACK PROJ INFO OPEN STATE
 	var descriptionOn = false;
@@ -107,19 +111,19 @@ var myTreehouseProjects = function() {
 		// MAKE RESPONSIVE NAV STICK ON SCROLL
 		if (width < 400) {
 			if (distance > 310 || distance2 > 310) {
-				arrowLeft.classList.toggle("arrowFixed", true);
-				arrowRight.classList.toggle("arrowFixed", true);
+				arrowWrap[0].classList.toggle("arrowFixed", true);
+				arrowWrap[1].classList.toggle("arrowFixed", true);
 			} else if (distance < 311 || distance2 < 311) {
-				arrowLeft.classList.toggle("arrowFixed", false);
-				arrowRight.classList.toggle("arrowFixed", false);
+				arrowWrap[0].classList.toggle("arrowFixed", false);
+				arrowWrap[1].classList.toggle("arrowFixed", false);
 			}
 		} else if (width > 400) {
-		if (distance > 265 || distance2 > 265) {
-				arrowLeft.classList.toggle("arrowFixed", true);
-				arrowRight.classList.toggle("arrowFixed", true);
-			} else if (distance < 266 || distance2 < 266) {
-				arrowLeft.classList.toggle("arrowFixed", false);
-				arrowRight.classList.toggle("arrowFixed", false);
+		if (distance > 300 || distance2 > 300) {
+				arrowWrap[0].classList.toggle("arrowFixed", true);
+				arrowWrap[1].classList.toggle("arrowFixed", true);
+			} else if (distance < 301 || distance2 < 301) {
+				arrowWrap[0].classList.toggle("arrowFixed", false);
+				arrowWrap[1].classList.toggle("arrowFixed", false);
 			}
 		}
 		
@@ -258,7 +262,70 @@ var myTreehouseProjects = function() {
 
 	projWrapHover();
 	//* ========================================= *//
+	
+	
+	
+	/*==============================================
+	MAKE PROJ IMG CLICKABLE
+	===============================================*/
+	var imgClicker = function() {
+		for (var i = 0; i < projInfo.length; i++) {
+			
+			// STICK A NUMBERED MARKER ON EACH PROJ INFO BUTTON
+			lgProjImage[i].marker = i;
+			smProjImage[i].marker = i;
+			
+			lgProjImage[i].onclick = function() {
+				window.open(projOpen[this.marker].getAttribute("href"));
+			};
+			
+			smProjImage[i].onclick = function() {
+				window.open(projOpen[this.marker].getAttribute("href"));
+			};
+		}
+	};
+	
+	imgClicker();
+	//* ========================================= *//
+	
+	
+	
+	/*==============================================
+	HELPER FUNCTION FOR TOGGLING CLASSES TO OPEN AND CLOSE INFO 
+	===============================================*/
+	var toggleInfoClasses = function() {
+		// LOOP THROUGH PROJS
+		for (var j = 0; j < projInfo.length; j++) {
 
+			// TOGGLE CLASS TO SHOW/HIDE ALL PROJ INFO AND TOGGLE TEXT ON ALL PROJ INFO BUTTON
+			if (!descriptionOn) {
+				projInfo[j].innerHTML = "Shrink <span class='book2 bookCover2'></span><span class='book2 bookBack2'></span>";
+				
+				projDescriptionWrap[j].classList.toggle("closeProjInfo", false);
+				projDescriptionWrap[j].classList.toggle("keepProjInfoClosed", false);
+				projDescriptionWrap[j].classList.toggle("keepProjInfoOpen", true);
+				
+				projTechWrap[j].classList.toggle("closeProjInfo", false);
+				projTechWrap[j].classList.toggle("keepProjInfoClosed", false);
+				projTechWrap[j].classList.toggle("keepProjInfoOpen", true);
+				
+			} else if (descriptionOn) {
+				projInfo[j].innerHTML = "View Project Info <span class='book2 bookCover2'></span><span class='book2 bookBack2'></span>";
+				
+				projDescriptionWrap[j].classList.toggle("openProjInfo", false);
+				projDescriptionWrap[j].classList.toggle("keepProjInfoOpen", false);
+				projDescriptionWrap[j].classList.toggle("keepProjInfoClosed", true);
+				
+				projTechWrap[j].classList.toggle("openProjInfo", false);
+				projTechWrap[j].classList.toggle("keepProjInfoOpen", false);
+				projTechWrap[j].classList.toggle("keepProjInfoClosed", true);
+				
+				// REMOVE FOCUS FROM PROJ INFO BUTTON
+				projInfo[j].blur();
+			}
+		}
+	};
+	
 
 
 	/*==============================================
@@ -274,37 +341,9 @@ var myTreehouseProjects = function() {
 
 			// OPEN AND CLOSE PROJ WRAP WITH LOGIC FOR ALL OCCASIONS
 			projInfo[i].onclick = function() {
-
-				// LOOP THROUGH PROJS
-				for (var j = 0; j < projInfo.length; j++) {
-
-					// TOGGLE CLASS TO SHOW/HIDE ALL PROJ INFO AND TOGGLE TEXT ON ALL PROJ INFO BUTTON
-					if (!descriptionOn) {
-						projInfo[j].innerHTML = "Shrink <span class='book2 bookCover2'></span><span class='book2 bookBack2'></span>";
-						
-						projDescriptionWrap[j].classList.toggle("closeProjInfo", false);
-						projDescriptionWrap[j].classList.toggle("keepProjInfoClosed", false);
-						projDescriptionWrap[j].classList.toggle("keepProjInfoOpen", true);
-						
-						projTechWrap[j].classList.toggle("closeProjInfo", false);
-						projTechWrap[j].classList.toggle("keepProjInfoClosed", false);
-						projTechWrap[j].classList.toggle("keepProjInfoOpen", true);
-						
-					} else if (descriptionOn) {
-						projInfo[j].innerHTML = "View Project Info <span class='book2 bookCover2'></span><span class='book2 bookBack2'></span>";
-						
-						projDescriptionWrap[j].classList.toggle("openProjInfo", false);
-						projDescriptionWrap[j].classList.toggle("keepProjInfoOpen", false);
-						projDescriptionWrap[j].classList.toggle("keepProjInfoClosed", true);
-						
-						projTechWrap[j].classList.toggle("openProjInfo", false);
-						projTechWrap[j].classList.toggle("keepProjInfoOpen", false);
-						projTechWrap[j].classList.toggle("keepProjInfoClosed", true);
-						
-						// REMOVE FOCUS FROM PROJ INFO BUTTON
-						projInfo[j].blur();
-					}
-				}
+				
+				// HELPER FUNCTION
+				toggleInfoClasses();
 				
 				// TOGGLE CLASS TO SHOW/HIDE CURRENT PROJ INFO AND TOGGLE TEXT ON CURRENT PROJ INFO BUTTON
 				if (!descriptionOn) {
@@ -321,6 +360,67 @@ var myTreehouseProjects = function() {
 	};
 
 	getThatInfoButton();
+	//* ========================================= *//
+	
+	
+	
+	/*==============================================
+	// MAKE "SKILLS" AND "ABOUT" HEADERS CLICKABLE FOR EASIER UI
+	===============================================*/
+	var skillsAndAbout = function() {
+
+		if (width > 624) {
+			
+			// LOOP THROUGH PROJS
+			for (var i = 0; i < projInfo.length; i++) {
+				
+				// STICK A NUMBERED MARKER ON EACH PROJ INFO BUTTON
+				projSectionTitleAbout[i].marker = i;
+				projSectionTitleSkills[i].marker = i;
+
+				// OPEN AND CLOSE PROJ WRAP WITH LOGIC FOR ALL OCCASIONS
+				projSectionTitleAbout[i].onclick = function() {
+					
+					// HELPER FUNCTION
+					toggleInfoClasses();
+					
+					// TOGGLE CLASS TO SHOW/HIDE CURRENT PROJ INFO AND TOGGLE TEXT ON CURRENT PROJ INFO BUTTON
+					if (!descriptionOn) {
+						projDescriptionWrap[this.marker].classList.toggle("openProjInfo", true);
+						projTechWrap[this.marker].classList.toggle("openProjInfo", true);
+						projInfo[this.marker].focus();
+						descriptionOn = true;
+					} else if (descriptionOn) {
+						projDescriptionWrap[this.marker].classList.toggle("closeProjInfo", true);
+						projTechWrap[this.marker].classList.toggle("closeProjInfo", true);
+						projInfo[this.marker].blur();
+						descriptionOn = false;
+					}
+				};
+				
+				projSectionTitleSkills[i].onclick = function() {
+					
+					// HELPER FUNCTION
+					toggleInfoClasses();
+					
+					// TOGGLE CLASS TO SHOW/HIDE CURRENT PROJ INFO AND TOGGLE TEXT ON CURRENT PROJ INFO BUTTON
+					if (!descriptionOn) {
+						projDescriptionWrap[this.marker].classList.toggle("openProjInfo", true);
+						projTechWrap[this.marker].classList.toggle("openProjInfo", true);
+						projInfo[this.marker].focus();
+						descriptionOn = true;
+					} else if (descriptionOn) {
+						projDescriptionWrap[this.marker].classList.toggle("closeProjInfo", true);
+						projTechWrap[this.marker].classList.toggle("closeProjInfo", true);
+						projInfo[this.marker].blur();
+						descriptionOn = false;
+					}
+				};
+			}
+		}
+	};
+
+	skillsAndAbout();
 	//* ========================================= *//
 };
 
